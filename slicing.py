3b09134316bf4f7f305942e489c52446ed525a85
mbeazer <mikebeazer@hotmail.com>
2021-10-31 14:34:33 -0700

    first4_last4 complete

--- slicing.py
+++ slicing.py
@@ -7,7 +7,7 @@ def every_other_removed(seq):
 
 
 def first4_last4_every_other_removed(seq):
-    pass
+    return seq[4:-4:2]
 
 
 def seq_reversed(seq):
@@ -18,6 +18,6 @@ def last_third_first_third_mid_third(seq):
     pass
 
 
-test_list = (1, 2, 3, 4, 5, 6, 7)
-print(every_other_removed('testing'))
-print(every_other_removed(test_list))
+test_list = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)
+print(first4_last4_every_other_removed('testing one two three four five'))
+print(first4_last4_every_other_removed(test_list))
